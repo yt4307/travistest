2504e991230a6a139dac2acadccd4bdb2632ccb5
Yeonhan Jung <yt4307@gmail.com>
2021-08-02 20:52:00 +0900

    yml 수정

--- test.js
+++ test.js
@@ -1,11 +1,1 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('Travis Hello')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+console.log("잘 되는지 확인좀");
